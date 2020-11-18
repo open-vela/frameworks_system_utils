39027d10aa6e24b5cc71839685b8d71dfa132b5e
Peter Bee <bijunda1@xiaomi.com>
2020-11-18 11:48:38 +0800

    framework/kvdb: Add kvdb APIs and CLI tools
    	APIs for setting and getting Key-Value pairs
    	CLI tools for command line debugging
    	changed API to Android style
    
    Signed-off-by: Peter Bee <bijunda1@xiaomi.com>

---
+++ setprop.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2020 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+
+#include "kvdb.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc == 3) {
+        int ret = property_set(argv[1], argv[2]);
+        if (ret < 0)
+            printf("Returned %d\n", ret);
+    } else if (argc == 2) {
+        int ret = property_delete(argv[1]);
+        if (ret < 0)
+            printf("Returned %d\n", ret);
+    } else
+        printf("\nUsage: %s <key> [value]\n", argv[0]);
+    return 0;
+}
