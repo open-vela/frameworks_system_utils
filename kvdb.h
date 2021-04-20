08f29383cf59590006e3a427a8d67bf40308f7ef
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-04-20 11:11:27 +0800

    frameworks/kvdb: Add Android compatible header file
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- kvdb.h
+++ kvdb.h
@@ -17,35 +17,6 @@
 #ifndef __KVDB_H
 #define __KVDB_H
 
-#include <stdint.h>
-
-#define PROP_KEY_MAX     100
-#define PROP_VALUE_MAX   100
-#define PROP_MSG_MAX     (3 + PROP_KEY_MAX + PROP_VALUE_MAX) /* +3 = +1(opcode) +2(len) */
-#define PROP_SERVER_PATH "kvdbd"
-
-#if defined(__cplusplus)
-extern "C"
-{
-#endif
-
-int property_set(const char* key, const char* value);
-int property_get(const char* key, char* value, const char* default_value);
-int property_delete(const char* key);
-
-typedef void (*property_callback)(const char* key, const char* value, void* cookie);
-int property_list(property_callback propfn, void* cookie);
-
-int property_set_bool(const char* key, int8_t value);
-int8_t property_get_bool(const char* key, int8_t default_value);
-int property_set_int32(const char* key, int32_t value);
-int32_t property_get_int32(const char* key, int32_t default_value);
-int property_set_int64(const char* key, int64_t value);
-int64_t property_get_int64(const char* key, int64_t default_value);
-int property_commit(void);
-
-#if defined(__cplusplus)
-}
-#endif
+#include <cutils/properties.h>
 
 #endif
