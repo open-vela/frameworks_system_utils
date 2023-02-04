d65190c6b20f8d31cc72c286be0e470f79331d22
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-02-05 04:36:41 +0800

    Move NuttX special funcion to kvdb.h
    and sync properties.h with Android 13
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- include/kvdb.h
+++ include/kvdb.h
@@ -19,4 +19,128 @@
 
 #include <cutils/properties.h>
 
+#define PROP_MSG_MAX       (3 + PROP_NAME_MAX + PROP_VALUE_MAX) /* +3 = +1(opcode) +2(len) */
+#define PROP_SERVER_PATH   "kvdbd"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+/**
+ * @brief Store Key-Values to database.
+ * @param[in] key entry key string
+ * @param[in] value entry value string
+ * @note Key starting with "persist." will be stored permanently after commit,
+ *   others will be lost after reboot.
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_set_oneway(const char* key, const char* value);
+
+/**
+ * @brief Delete a KV pair by key.
+ * @param[in] key entry key string
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_delete(const char* key);
+
+/**
+ * @brief Actively commit all property changes.
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_commit(void);
+
+/**
+ * @brief Reload default property value.
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_reload(void);
+
+/**
+ * @brief Wait the monitored key until its value updated or key deleted
+ * @param[in] key the monitored key string, support fnmatch pattern
+ * @param[in] timeout the wait timeout time (in milliseconds)
+ * @param[out] newkey pointer to a string buffer to receive the key of
+ *                    the updated/deleted value
+ * @param[out] newvalue pointer to a string buffer to receive the updated
+ *                      value or deleted value ('\0')
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_wait(const char* key, char* newkey, char* newvalue, int timeout);
+
+/**
+ * @brief Open a key monitor channel
+ * @param[in] key the monitored key string, support fnmatch pattern
+ * @return On success returns a file descriptor, -errno otherwise.
+ */
+int property_monitor_open(const char* key);
+
+/**
+ * @brief Read the monitored key and value
+ * @param[in] fd file descriptor returned by property_monitor_open()
+ * @param[out] newkey pointer to a string buffer to receive the key of
+ *                    the updated/deleted value
+ * @param[out] newvalue pointer to a string buffer to receive the updated
+ *                      value or deleted value ('\0')
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_monitor_read(int fd, char* newkey, char* newvalue);
+
+/**
+ * @brief Close a key monitor channel
+ * @param[in] fd file descriptor returned by property_monitor_open()
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_monitor_close(int fd);
+
+/**
+ * @brief Saves a boolean to database.
+ * @param[in] key entry key string
+ * @param[in] value entry boolean value
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_set_bool(const char* key, int8_t value);
+int property_set_bool_oneway(const char* key, int8_t value);
+
+/**
+ * @brief Saves an 32-bit integer to database.
+ * @param[in] key entry key string
+ * @param[in] value entry value
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_set_int32(const char* key, int32_t value);
+int property_set_int32_oneway(const char* key, int32_t value);
+
+/**
+ * @brief Saves an 64-bit integer to database.
+ * @param[in] key entry key string
+ * @param[in] value entry value
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_set_int64(const char* key, int64_t value);
+int property_set_int64_oneway(const char* key, int64_t value);
+
+/**
+ * @brief Saves a binary buffer to database.
+ * @param[in] key entry key string
+ * @param[in] value buffer value
+ * @param[in] size entry size
+ * @return On success returns 0, -errno otherwise.
+ */
+int property_set_buffer(const char* key, const void* value, size_t size);
+int property_set_buffer_oneway(const char* key, const void* value, size_t size);
+
+/**
+ * @brief Retrieve a Key-Value from database and interpret as binary buffer.
+ * @param[in] key entry key string
+ * @param[in] value buffer value
+ * @param[in] size buffer size
+ * @return On success returns the array length, -errno otherwise.
+ */
+ssize_t property_get_buffer(const char* key, void* value, size_t size);
+
+#if defined(__cplusplus)
+}
+#endif
+
 #endif
