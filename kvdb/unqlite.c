daf0204ba96ce64d558e28c7952ed04c2e119b9e
guohao15 <guohao15@xiaomi.com>
2025-06-05 21:26:48 +0800

    kvdb:dump more information when API failed
    Signed-off-by: guohao15 <guohao15@xiaomi.com>

--- kvdb/unqlite.c
+++ kvdb/unqlite.c
@@ -40,15 +40,24 @@ struct kvdb {
 
 int kvdb_persist_set(struct kvdb* kvdb, const char* key, size_t key_len, const void* value, size_t val_len, bool force)
 {
-    return unqlite_kv_store(kvdb->db, key, key_len, value, val_len);
+    int ret = unqlite_kv_store(kvdb->db, key, key_len, value, val_len);
+    if (ret < 0) {
+        KVERR("kv_store key:%s key_len:%d error %d!\n", key, (int)key_len, ret);
+        return ret;
+    }
+
+    return ret;
 }
 
 ssize_t kvdb_persist_get(struct kvdb* kvdb, const char* key, size_t key_len, void* value, size_t val_len)
 {
     unqlite_int64 val_size = val_len;
     ssize_t ret = unqlite_kv_fetch(kvdb->db, key, key_len, value, &val_size);
-    if (ret < 0)
+    if (ret < 0) {
+        if (ret != UNQLITE_NOTFOUND)
+            KVERR("kv_fetch key:%s key_len:%d error %d!\n", key, (int)key_len, ret);
         return ret;
+    }
 
     if (val_size <= 0)
         return -EINVAL;
@@ -58,7 +67,13 @@ ssize_t kvdb_persist_get(struct kvdb* kvdb, const char* key, size_t key_len, voi
 
 int kvdb_persist_delete(struct kvdb* kvdb, const char* key, size_t key_len)
 {
-    return unqlite_kv_delete(kvdb->db, key, key_len);
+    int ret = unqlite_kv_delete(kvdb->db, key, key_len);
+    if (ret < 0) {
+        KVERR("kv_delete key:%s key_len:%d error %d!\n", key, (int)key_len, ret);
+        return ret;
+    }
+
+    return ret;
 }
 
 static int kvdb_list_value(const void* value, unsigned int len, void* arg)
@@ -70,10 +85,17 @@ static int kvdb_list_value(const void* value, unsigned int len, void* arg)
 
 static int kvdb_list_key(const void* value, unsigned int len, void* arg)
 {
+    int ret;
     kvdb_consume_data* data = arg;
     data->key = value;
     data->key_len = len;
-    return unqlite_kv_cursor_data_callback(data->cur, kvdb_list_value, data);
+    ret = unqlite_kv_cursor_data_callback(data->cur, kvdb_list_value, data);
+    if (ret < 0) {
+        KVERR("kv_cursor_data_callback failed error %d!\n", ret);
+        return ret;
+    }
+
+    return ret;
 }
 
 int kvdb_persist_list(struct kvdb* kvdb, kvdb_consume consume, void* cookie)
@@ -91,6 +113,7 @@ int kvdb_persist_list(struct kvdb* kvdb, kvdb_consume consume, void* cookie)
     while (unqlite_kv_cursor_valid_entry(cur)) {
         int ret = unqlite_kv_cursor_key_callback(cur, kvdb_list_key, &data);
         if (ret < 0) { /* exit loop demanded by consume */
+            KVERR("kv_cursor_key_callback failed error %d!\n", ret);
             unqlite_kv_cursor_release(kvdb->db, cur);
             return ret;
         }
@@ -131,8 +154,10 @@ int kvdb_persist_init(struct kvdb** kvdb)
 
     ret = unqlite_open(&(*kvdb)->db, CONFIG_KVDB_PERSIST_PATH, UNQLITE_OPEN_CREATE | UNQLITE_OPEN_OMIT_JOURNALING);
 
-    if (ret < 0)
+    if (ret < 0) {
+        KVERR("unqlite_open failed error %d!\n", ret);
         free(*kvdb);
+    }
 
     return ret;
 }
